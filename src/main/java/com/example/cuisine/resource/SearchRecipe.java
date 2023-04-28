f916baa7a2e7af5cb559c7eb159e309f8a27fc99
Amina Cherni <cherniamina21@gmail.com>
2023-04-28 09:59:16 +0200

    API

--- src/main/java/com/example/cuisine/resource/SearchRecipe.java
+++ src/main/java/com/example/cuisine/resource/SearchRecipe.java
@@ -1,4 +1,16 @@
 package com.example.cuisine.resource;
 
+import com.example.cuisine.model.Recipe;
+import com.example.cuisine.service.RecipeService;
+
+@Path ("recipes")
 public class SearchRecipe {
+    RecipeService recipeService = new RecipeService();
+    @GET
+    @Produces(value = MediaType.APPLICATION_JSON)
+    public Response findRandomLastSixDayRecipes(){
+        Recipe randomRecipe = recipeService.fetchRandomLastSixDayRecipes();
+        RecipeDto recipeDto = randomRecipe.toDto();
+        return Response.ok(recipeDto).build();
+    }
 }
